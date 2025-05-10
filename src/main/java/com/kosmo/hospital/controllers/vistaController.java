885552c43e46a4dff3524dbe6456cbcdc1f2c526
Othon hernandez <cani.rap.3@hotmail.com>
2025-05-09 18:26:17 -0600

    ajuste

---
+++ src/main/java/com/kosmo/hospital/controllers/vistaController.java
@@ -0,0 +1,20 @@
+package com.kosmo.hospital.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/citas")
+
+public class vistaController {
+    @GetMapping("/consulta")
+    public String mostrarConsulta() {
+        return "hospital"; // busca consultaCitas.jsp en /WEB-INF/jsp/
+    }
+
+    @GetMapping("/agregar")
+    public String mostrarFormularioAgregar() {
+        return "agregarCita"; // busca agregarCita.jsp en /WEB-INF/jsp/
+    }
+}
--- src/main/webapp/WEB-INF/jsp/Hospital.jsp
+++ src/main/webapp/WEB-INF/jsp/Hospital.jsp
@@ -6,12 +6,12 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
 <html>
 <title>Consultar Citas</title>
 <script>
     function cargarCitas() {
-        fetch('/api/citas')
+        fetch('/api/citas/getcitas')
             .then(response => response.json())
     .then(data => {
             const tabla = document.getElementById('tablaCitas');
