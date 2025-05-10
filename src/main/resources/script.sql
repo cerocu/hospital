a0ce60ab8a72f94f436b2dd2e1cbf093e7f88c5d
Othon hernandez <cani.rap.3@hotmail.com>
2025-05-09 18:34:59 -0600

    add sql script

---
+++ src/main/resources/script.sql
@@ -0,0 +1,4 @@
+INSERT INTO `doctores` VALUES (1,'garcia','lopez','cirujano','pepe'),(2,'garcia','perez','cirujano','carlos'),(3,'garcia','roberto','cirujano','ricardo'),(4,'juarez','lopez','cirujano','pedro'),(5,'juarez','garcia','cirujano','mario'),(6,'lopez','garcia','cirujano','jose');
+
+
+INSERT INTO `doctores` VALUES (1,'garcia','lopez','cirujano','pepe'),(2,'garcia','perez','cirujano','carlos'),(3,'garcia','roberto','cirujano','ricardo'),(4,'juarez','lopez','cirujano','pedro'),(5,'juarez','garcia','cirujano','mario'),(6,'lopez','garcia','cirujano','jose');
